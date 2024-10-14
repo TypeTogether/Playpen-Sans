bad154946a089545c1484bbfe09761225aa9fcdb
Joancarles Casasín <joanca@casasin.com>
2024-10-14 20:02:38 +0200

    dev deva .fea

--- sources/scripts/bump-version-minor.py
+++ sources/scripts/bump-version-minor.py
@@ -1,11 +1,11 @@
 import os.path
 from glyphsLib import GSFont
 
-gpath = os.path.abspath(os.path.relpath("../PlaypenSans.glyphspackage"))
+gpath = os.path.abspath(os.path.relpath("../sources/PlaypenSans.glyphspackage"))
 
 font = GSFont(gpath)
 font.versionMinor += 1
 print(f"Bumped version: v{font.versionMajor}.{font.versionMinor}")
 
-font.save(gpath)
+font.save(font.filepath)
 
